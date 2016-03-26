f59051da47077460beb92bc1bca070ff2c4f1988
Aaron Freidus <aaronf101@gmail.com>
2016-03-26 11:27:16 -0400

    [When commited] can solve single digit replacement, but is primed to do multiple digit.

--- numToText.js
+++ numToText.js
@@ -14,5 +14,47 @@ Ex: numToText("I have 5 dogs and 6 ponies"); // returns "I have five dogs and si
 */
 
 var numToText = function(str) {
+  var numbersOneToNine = ['one','two','three','four','five','six','seven','eight','nine'];
+  var ans = [];
+  var array = str.split(' ');
+  var recur = function(arr){
+  	if(arr.length == 0){
+  		return ans;
+  	}else{
+  	  var temp = arr[0];
+  	  if(!isNaN(temp)){
+  		  ans.push(numbersOneToNine[temp]);
+          recur(arr.slice(1,arr.length));
+      }else{
+      	ans.push(arr[0]);
+        recur(arr.slice(1,arr.length));
+      }
+    }
+  }
+  recur(array);
+  return ans.join(' ');
+}
 
+
+var numToText2 = function(str) {
+	var numbersOneToNine = ['one','two','three','four','five','six','seven','eight','nine'];
+	var ans = '';
+	if(str.length === 0){
+		return ans;
+	}else{
+	  var tempStr = str.split(' ');
+    
+	  if(!isNaN(tempStr[0])){
+	 	ans = ans + ' ' + numbersOneToNine[tempStr[0]];
+	  }else{
+	 	ans = ans + ' ' + tempStr[0];
+	  }
+    }
+    str = tempStr.slice(1,tempStr.length).join(' ');
+    return ans + numToText2(str);
 };
+
+console.log(numToText("I have 5 dogs and 6 ponies"));
+
+console.log(numToText2("I have 5 dogs and 6 ponies"));
+
